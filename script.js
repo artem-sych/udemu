6d8f0d8eda98f75687c7c4a5fc886c4e4d98d38e
unknown <artem.sych1989@gmail.com>
2021-10-15 15:27:49 +0300

    add function showMyDB and wrwrireYourGenres

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+//start();
+
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -7,37 +18,50 @@ let personalMovieDB = {
     privat: false
 };
 
-
-if (personalMovieDB.count > 0 && personalMovieDB.count < 10) {
-    alert('Вы посмотрели слишком мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    alert('Вы классический зритель');
-} else if (personalMovieDB.count > 30) {
-    alert('Вы киноман');
-} else {
-    alert('Произошла ошибка');
-}
-
-for (let i = 0; i < 2; i++) {
-    let a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?');
-    if (a !== null && b !== null && a !== '' && b !== '' && a !== ' ' && b !== ' ' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
+function remeberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?');
+        if (a !== null && b !== null && a !== '' && b !== '' && a !== ' ' && b !== ' ' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
     }
 }
 
+//remeberMyFilms();
+
+function detecPersonalLevel() {
+    if (personalMovieDB.count > 0 && personalMovieDB.count < 10) {
+        alert('Вы посмотрели слишком мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        alert('Вы классический зритель');
+    } else if (personalMovieDB.count > 30) {
+        alert('Вы киноман');
+    } else {
+        alert('Произошла ошибка');
+    }
+}
+
+//detecPersonalLevel();
+
+function showMyDB() {
+    if (personalMovieDB.privat === false) {
+        console.log(personalMovieDB);
+    }
+}
+
+function wrireYourGenres() {
 
 
+    for (let i = 1; i < 4; i++) {
+        personalMovieDB.genres.push(prompt(`Ваш любимый жанр под номером ${i}`));
+    }
+}
 
-
-
-
-
-
-
+wrireYourGenres();
 
 console.log(personalMovieDB);
